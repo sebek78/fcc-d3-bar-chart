2cc540df779b253817c4a38ca4463c33d37aaedb
sebek78 <sebasporek@gmail.com>
2017-07-30 21:30:34 +0200

    changing chart bar display method from div to SVG

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const url ='https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 let data = [];
 
+let maxWidth = 800;
+let barHeight = 10;
+
 fetch(url)
   .then((resp)=> resp.json())
   .then(function(receivedData) {
@@ -8,14 +11,28 @@ fetch(url)
       console.log(data[0]);
       let maxX = d3.max(data);
       let max = maxX[1];
-      console.log("max: ",d3.max(data), "maxX: ", maxX[1]);
-      let x =  d3.scaleLinear().domain([0,max]).range([0,800]);
-      const chart = d3.select(".chart");
-      const bar = chart.selectAll("div");
-      let barUpdate = bar.data(data);
-      let barEnter = barUpdate.enter().append("div");
-      barEnter.style("width", function(d) {return x(d[1])+"px"});
-      barEnter.text(function(d) { return d; });
+      console.log("maXx: ",d3.max(data), "max: ", maxX[1]);
+      let x =  d3.scaleLinear().domain([0,max]).range([0,maxWidth]);
+
+      let chart = d3.select(".chart")
+            .attr("width",maxWidth)
+            .attr("height", barHeight*data.length);
+
+      let bar = chart.selectAll("g")
+          .data(data)
+          .enter().append("g")
+          .attr("transform", function(d, i){ return "translate(0,"+i*barHeight+")"; });
+
+      bar.append("rect")
+          .attr("width",function(d){ return x(d[1]); })
+          .attr("height", barHeight-1);
+
+      bar.append("text")
+          .attr("x", function(d){ return x(d[1])-3; })
+          .attr("y", barHeight/2)
+          .attr("dy", "0.35em")
+          .text(function(d){ return d; });
+
   })
   .catch(function(error){
     console.log(error)
